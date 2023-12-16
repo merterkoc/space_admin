eac1231fe2273b384e90b038bd746275e31af560
mertyasarerkoc <mertyasarerkocc@gmail.com>
2023-12-16 03:17:20 +0300

    Header fix.

--- lib/core/dio/http_handler/default_http_handler.dart
+++ lib/core/dio/http_handler/default_http_handler.dart
@@ -10,6 +10,8 @@ class DefaultHttpHeaderInterceptor extends Interceptor {
   ) {
     options.headers.addAll({
       'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+      'Access-Control-Allow-Methods': 'GET,PUT,PATCH,POST,DELETE',
     });
     handler.next(options);
   }
